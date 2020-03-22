2d590db457eeca0ffdf33e6bcc08b51a67c55c8f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-22 17:47:47 +0100

    working chart

--- components/Charts.js
+++ components/Charts.js
@@ -1,52 +1,52 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Image, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
 } from "react-native-chart-kit";
 import {Dimensions} from "react-native-web";
 
 const line = {
-  labels: ["January", "February", "March", "April", "May", "June"],
-  datasets: [
-    {
-      data: [20, 45, 28, 80, 99, 43],
-      strokeWidth: 2
-    }
-  ]
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+        {
+            data: [20, 45, 28, 80, 99, 43],
+            strokeWidth: 2
+        }
+    ]
 };
 
 const Charts = () => {
-  return (
-    <View>
-      <Text>Charts</Text>
-      <LineChart
-        data={line}
-        width={Dimensions.get("window").width}
-        height={220}
-        yAxisLabel={"$"}
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16
-          }
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16
-        }}
-      ></LineChart>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Charts</Text>
+            <LineChart
+                data={line}
+                width={Dimensions.get("window").width}
+                height={220}
+                yAxisLabel={"$"}
+                chartConfig={{
+                    backgroundColor: "#e26a00",
+                    backgroundGradientFrom: "#fb8c00",
+                    backgroundGradientTo: "#ffa726",
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    style: {
+                        borderRadius: 1
+                    }
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 1
+                }}
+            ></LineChart>
+        </View>
+    );
 };
 
 export default Charts;
